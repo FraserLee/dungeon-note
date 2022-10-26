9209a65ac7ef6c6acb1046e2718974ee25b6e8dc
Fraser <tomatoesneverlie@gmail.com>
2022-10-26 02:56:17 -0400

    basic front-end framework running

--- dungeon-parse/src/lib.rs
+++ dungeon-parse/src/lib.rs
@@ -1,11 +1,40 @@
+
+
+
+
 pub fn parse(input: String) -> String {
-    const HTML_START: &str = r#"
-    <!DOCTYPE html>
-    <html lang="en">
-        <head><title>Dungeon Note 3</title></head>
-        <body>"#;
-    const HTML_END: &str = r#"
-        </body>
-    </html>"#;
-    return format!("{}{}{}", HTML_START, input, HTML_END);
+    const HTML: &str = r#"
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"><title>Dungeon Note 3</title></head>
+<body>
+  <script type="module">
+
+    import { h, Component, render } from 'https://unpkg.com/preact@latest?module';
+    import { useState, useEffect, useCallback } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
+
+    import htm from 'https://unpkg.com/htm?module';
+    // Initialize htm with Preact
+    const html = htm.bind(h);
+
+    function App (props) {
+      const [count, setCount] = useState(0);
+      const increment = useCallback(() => {
+        setCount(count + 1);
+      }, [count]);
+      return html`
+      <div>
+          <h1>Clicked ${count} times</h1>
+          <button onClick=${increment}>Click me</button>
+      </div>
+      <br>
+      `;
+
+    }
+
+    render(html`<${App} name="World" />`, document.body);
+  </script>
+</body>
+</html>"#;
+    return format!("{}{}", HTML, input[0..400].to_string());
 }
