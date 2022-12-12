71335159468410061fd6e56495087fd3860185c0
Fraser <tomatoesneverlie@gmail.com>
2022-12-11 23:18:42 -0500

    "Loading" variant

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -2,24 +2,24 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
+import Http
+import Json.Decode exposing (Decoder, map4, field, int, string)
 
-type alias Model = { textBoxes : List String }
-init = { textBoxes = ["foo", "bar"] }
+type Model = Loading | Loaded (List String)
 type Msg = None
 
+init = Loading
+
 update : Msg -> Model -> Model
-update msg model = model
+update _ model = model
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "elm test." ],
-          div []
-            (List.map
-                (\t -> p [] [ text t ])
-                model.textBoxes
-            )
-        ]
+    case model of
+        Loading -> text "Loading..."
+        Loaded textBoxes ->
+            let textBoxesHtml = List.map (\t -> p [] [ text t ]) textBoxes
+                header = h1 [] [ text "elm test." ]
+            in div [] [ header, div [] textBoxesHtml ]
 
 main = Browser.sandbox { init = init, update = update, view = view }
-
