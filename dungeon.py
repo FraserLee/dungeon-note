845c73f492af6acf0e47eb08bcb13c080884b824
Fraser <tomatoesneverlie@gmail.com>
2022-12-11 21:00:12 -0500

    added a pathway somewhat more like the real thing

--- dungeon.py
+++ dungeon.py
@@ -1,11 +1,15 @@
-# host a webpage on port 3000. The webpage will have a button that, when
-# clicked, will send a request to the server to increment a counter.
-# The server will then send back the new value of the counter, which will
-# be displayed on the webpage.
-
 import http.server
 import json
 import sys
+import dataclasses
+
+@dataclasses.dataclass
+class Text:
+    text   : str
+    x      : int
+    y      : int
+    width  : int
+    height : int
 
 # The Html and Javascript code for the webpage that will be served
 html = """
@@ -23,38 +27,84 @@ html = """
                 document.getElementById('counter').innerHTML = data.counter;
             });
         }
+
+        function refreshTextBoxes() {
+            fetch('/text', {method: 'GET'})
+            .then(response => response.json())
+            .then(data => {
+                // Create a bunch of <p> elements with each text box
+                let textRoot = document.getElementById('text');
+
+                // clear existing text boxes
+                textRoot.innerHTML = '';
+                for (let i = 0; i < data.text.length; i++) {
+                    let text = data.text[i];
+                    let p = document.createElement('p');
+                    p.innerHTML = text.text;
+                    textRoot.appendChild(p);
+                }
+            });
+        }
+
+        // after the page loads, call refreshTextBoxes() to get the initial setup
+        window.onload = refreshTextBoxes;
+
     </script>
 </head>
 <body>
     <h1>Counter</h1>
     <p id="counter">0</p>
     <button onclick="increment()">Increment</button>
+    <h1>Text</h1>
+    <div id="text"></div>
 </body>
 </html>
 """
 
+print("serving at http://localhost:3000")
+
 # The counter that will be incremented by the webpage
 counter = 0
+# The text boxes that will be displayed on the webpage
+text = [Text('Hello', 0, 0, 100, 100), Text('World', 100, 100, 100, 100)]
 
 # The handler for the webpage
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Serve the webpage
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(html.encode())
+        match self.path:
+            case '/':
+                # Send the HTML code for the webpage
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(html.encode())
+
+            case '/text':
+                # Send the textboxes
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({'text': [dataclasses.asdict(t) for t in text]}).encode())
 
     def do_POST(self):
-        # Increment the counter
-        global counter
-        counter += 1
-        print('Counter:', counter)
-        # Send the new value of the counter back to the webpage
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps({'counter': counter}).encode())
+        match self.path:
+            case '/increment':
+                # Increment the counter
+                global counter
+                counter += 1
+                print('Counter:', counter)
+                # Send the new value back to the webpage
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({'counter': counter}).encode())
+
+            case _:
+                # Send an error message back to the webpage
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write('Not found'.encode())
 
 # Start the server, and quit when Ctrl+C is pressed
 try:
--- readme.md
+++ readme.md
@@ -1,2 +1,23 @@
 Current plan - quickly scaffold this idea out using python, swap to something
 more sustainable once the skeleton is in place.
+
+- [x] Python <-> Locally hosted webpage
+- [x] Send forwards json full of dummy textboxes
+- [x] Render that on the page
+- [ ] Move HTML into its own file
+- [ ] Re-add styling from dungeon-note-2
+- [ ] Click textbox to entire bounding mode edit (corners, drag and drop, etc)
+- [ ] Have changes get pushed backwards to python
+- [ ] Swap to elm for frontend
+
+- [ ] Watch file in python, grab list of textboxes from there
+- [ ] Save Changes when made
+- [ ] Proper Markdown parsing
+- [ ] Re-add menu bar
+- [ ] Lines
+- [ ] Math
+- [ ] Write tests
+- [ ] find out other things that should be on this list.
+
+
+
