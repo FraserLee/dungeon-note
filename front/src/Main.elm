faaf266fb2fe55dacf8af034ac25a57200f6e69b
Fraser <tomatoesneverlie@gmail.com>
2022-12-16 15:10:38 -0500

    better types

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -3,17 +3,18 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Http
-import Json.Decode exposing (Decoder, map4, field, int, string)
+import Json.Decode exposing (Decoder, field, int, string)
 
-type Model = Loading | Loaded (List String)
-type Msg = GotData (Result Http.Error (List String))
+type alias Document = { paragraphs : List String }
+
+type Model = Loading | Loaded Document | Failed Http.Error
+type Msg = GotData (Result Http.Error (Document))
 
 fetchData : Cmd Msg
-fetchData = Http.get { url = "/fetch", expect = Http.expectJson GotData decodeData }
+fetchData = Http.get { url = "/fetch", expect = Http.expectJson GotData decodeDocument }
 
--- get from JSON "text" field - a list of strings
-decodeData : Decoder (List String)
-decodeData = field "text" (Json.Decode.list string)
+decodeDocument : Decoder Document
+decodeDocument = field "text" (Json.Decode.list string) |> Json.Decode.map Document
 
 init : () -> (Model, Cmd Msg)
 init _ = (Loading, fetchData)
@@ -22,15 +23,16 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         GotData (Ok data) -> (Loaded data, Cmd.none)
-        GotData (Err _) -> (Loaded ["some error"], Cmd.none)
+        GotData (Err err) -> (Failed err, Cmd.none)
 
 view : Model -> Html Msg
 view model =
     case model of
         Loading -> text "Loading..."
-        Loaded textBoxes ->
-            let textBoxesHtml = List.map (\t -> p [] [ text t ]) textBoxes
+        Loaded d ->
+            let textBoxesHtml = List.map (\t -> p [] [ text t ]) d.paragraphs
                 header = h1 [] [ text "elm test." ]
             in div [] [ header, div [] textBoxesHtml ]
+        Failed err -> text "Failed to load data."
 
 main = Browser.element { init = init, update = update, view = view, subscriptions = \_ -> Sub.none }
