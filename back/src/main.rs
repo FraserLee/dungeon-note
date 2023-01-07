7b46145cbfc57f37947b65f40a47400da420f71a
Fraser <tomatoesneverlie@gmail.com>
2023-01-07 16:15:52 -0500

    stripped out most of the aux stuff

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,66 +1,52 @@
-use warp::{sse, Filter};
+// MIN FAILING SAMPLE. 
+// - Manually create a HTML page in a string that'll do the alert() thing. 
+// - Possibly include timer one to show how working looks.
+// - see if I can get it working with just a manual yield in the stream macro, not even using the watcher.
 
-use serde::{Serialize, Deserialize};
-
-use lazy_static::lazy_static;
-
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::convert::Infallible;
+use std::path::Path;
+use std::time::Duration;
 
 use async_stream::stream;
+use futures::{ channel::mpsc::{channel, Receiver}, SinkExt, };
 use futures_util::stream::StreamExt;
-use futures_util::pin_mut;
-use std::convert::Infallible;
-use std::time::Duration;
-use tokio::time::interval;
-use tokio_stream::wrappers::IntervalStream;
-use futures_core::stream::Stream;
-use futures::{
-    channel::mpsc::{channel, Receiver},
-    SinkExt,
-};
-use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher, Config};
-use std::path::Path;
+use warp::{sse, Filter};
 
-// create server-sent event
-fn sse_counter(counter: u64) -> Result<sse::Event, Infallible> {
-    Ok(sse::Event::default().data(counter.to_string()))
+use notify::{ Event, RecommendedWatcher, RecursiveMode, Watcher, Config };
+
+// index.html string
+const INDEX_HTML: &str = r#"
+<!DOCTYPE HTML>
+<html>
+<head> <meta charset="UTF-8"> </head>
+
+<body>
+  <h1>Warp SSE Push on File Change</h1>
+  <script> 
+      var tickSource = new EventSource("/tick");
+      var fileChangeSource = new EventSource("/file_change");
+
+      tickSource.onmessage = (event) => {
+          document.body.innerHTML += "<p>tick event</p>";
+      };
+
+      fileChangeSource.onmessage = (event) => {
+          document.body.innerHTML += "<p>file change event</p>";
+      };
+  </script>
+</body>
+</html>
+"#;
+
+
+// simple SSE event
+fn sse_event() -> Result<sse::Event, Infallible> {
+    Ok(sse::Event::default().data(""))
 }
 
+// async notify stuff
 
-// -- document data ------------------------------------------------------------
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Text {
-    text: String,
-    x: f64,
-    y: f64,
-    width: f64,
-}
-
-lazy_static! {
-    static ref doc_path: String = std::env::args().nth(1).unwrap();
-    static ref front_path: String = std::env::args().nth(2).unwrap();
-    static ref DOCUMENT: Arc<Mutex<HashMap<String, Text>>> = {
-        let mut document = HashMap::new();
-        document.insert("foo".to_string(), Text {
-            text: "Hello ".repeat(50),
-            x: -300.0,
-            y: 0.0,
-            width: 600.0,
-        });
-        document.insert("bar".to_string(), Text {
-            text: "World ".repeat(30),
-            x: 100.0,
-            y: 300.0,
-            width: 300.0,
-        });
-        Arc::new(Mutex::new(document))
-    };
-}
-
-// -- watch for changes --------------------------------------------------------
-
+// https://github.com/notify-rs/notify/blob/e375fcefd23edd23e7138d8b3a97a721d6b7bbca/examples/async_monitor.rs#L22
 fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
     let (mut tx, rx) = channel(1);
 
@@ -73,114 +59,35 @@ fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Resul
     Ok((watcher, rx))
 }
 
-fn async_watcher_stream() -> impl Stream<Item = ()> {
-    let (mut watcher, mut rx) = async_watcher().unwrap();
-    watcher.watch(Path::new(&*doc_path), RecursiveMode::Recursive).unwrap();
-
-    let stream = stream! {
-        while let Some(res) = rx.next().await {
-            match res {
-                Ok(event) => {
-                    println!("event: {:?}", event);
-                    yield;
-                }
-                Err(e) => {
-                    println!("watch error: {:?}", e);
-                }
-            }
-        }
-    };
-
-    stream
-}
-
-
 // -- main ---------------------------------------------------------------------
 
-#[tokio::main]
-async fn main() {
-    let watch_path = Path::new(&*doc_path);
+async fn sse_file_watch() {
+    let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
 
-    println!("doc path: {}", &*doc_path);
+    // send an sse event on /tick every 3 seconds
+    let tick_sse  = warp::path("tick").and(warp::get()).map(|| {
 
+        let stream = stream! {
+            loop {
+                tokio::time::sleep(Duration::from_secs(3)).await;
+                println!("tick");
+                yield sse_event();
+            }
+        };
 
-    // futures::executor::block_on(async {
-    //     let (mut watcher, mut rx) = async_watcher().unwrap();
-    //     watcher.watch(watch_path, RecursiveMode::Recursive).unwrap();
-    //     while let Some(res) = rx.next().await {
-    //         match res {
-    //             Ok(event) => println!("changed: {:?}", event),
-    //             Err(e) => println!("watch error: {:?}", e),
-    //         }
-    //     }
-    // });
+        warp::sse::reply(warp::sse::keep_alive().stream(stream))
 
-    // -- routes ---------------------------------------------------------------
+    });
 
-    // GET / => front_path/index.html
-    let front = warp::path::end().and(warp::fs::file(front_path.clone() + "/index.html"));
-    // GET /fetch => send json encoded document
-    let fetch = warp::path("fetch").map(|| warp::reply::json(&*DOCUMENT.lock().unwrap()));
-    // GET /<path> => front_path/<path>
-    let static_files = warp::fs::dir(front_path.clone() + "/");
-
-    let update = warp::path!("update" / String)
-        .and(warp::body::json())
-        .map(|key: String, text: Text| {
-            DOCUMENT.lock().unwrap().insert(key, text);
-            warp::reply()
-        });
-
-    // let (mut watcher, mut rx) = async_watcher().unwrap();
-    //
-    // watcher.watch(watch_path, RecursiveMode::Recursive).unwrap();
-    //
-    // let stream = async_stream::stream! {
-    //     while let Some(res) = rx.next().await {
-    //         println!("AAAA");
-    //         match res {
-    //             Ok(event) => { println!("changed: {:?}", event); },
-    //             Err(e) => { println!("watch error: {:?}", e); },
-    //         }
-    //         yield sse_counter(3);
-    //     }
-    // };
-    //
-
-    // let refresh = warp::path("refresh").map(|| {
-    //     async_watcher_stream()
-    // })
-    // .map(|stream| stream)
-    // .and(warp::sse())
-    // .map(|stream, sse| {
-    //     let stream = stream.map(|_| sse_counter(3));
-    //     sse.reply(sse::keep_alive().stream(stream))
-    // });
-
-
-
-    // pin_mut!(stream);
-
-    let refresh = warp::path("refresh").and(warp::get()).map(|| {
-    //     warp::sse::reply(warp::sse::keep_alive().stream(stream))
-
-
-        // // create server event source
-        // let interval = interval(Duration::from_secs(10));
-        // let stream = IntervalStream::new(interval);
-        // let event_stream = stream.map(move |_| {
-        //     dbg!("tick");
-        //     sse_counter(0)
-        // });
-        // // reply using server-sent events
-        // warp::sse::reply(event_stream)
+    // sse event on /file_change every time the file changes
+    let file_change_sse = warp::path("file_change").and(warp::get()).map(|| {
 
         let (mut watcher, mut rx) = async_watcher().unwrap();
 
-        // Add a path to be watched. All files and directories at that path and
-        // below will be monitored for changes.
-        watcher.watch(Path::new(&*doc_path), RecursiveMode::Recursive).unwrap();
-
+        watcher.watch(
+                Path::new(&*std::env::args().nth(1).unwrap()), 
+                RecursiveMode::Recursive
+            ).unwrap();
 
         let stream = stream! {
             while let Some(res) = rx.next().await {
@@ -188,23 +95,40 @@ async fn main() {
                     Ok(event) => { println!("event: {:?}", event); }
                     Err(e) => { println!("watch error: {:?}", e); }
                 }
-                yield sse_counter(0);
+                yield sse_event();
             }
         };
 
-
         warp::sse::reply(stream)
-
     });
-
-    let routes = front.or(fetch).or(static_files).or(update).or(refresh);
 
     // -- run server -----------------------------------------------------------
 
-    println!(" --------------------------------------");
-    println!(" -- serving at http://localhost:3100 --");
+    println!(" -- serving at http://localhost:3000 --");
     println!(" -------- press ctrl-c to stop --------");
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3100)).await;
-
+    warp::serve(index.or(tick_sse).or(file_change_sse)).run(([127, 0, 0, 1], 3000)).await;
 }
+
+async fn local_file_watch() {
+    let (mut watcher, mut rx) = async_watcher().unwrap();
+
+    watcher.watch(
+            Path::new(&*std::env::args().nth(1).unwrap()), 
+            RecursiveMode::Recursive
+        ).unwrap();
+
+    loop {
+        if let Some(res) = rx.next().await {
+            match res {
+                Ok(event) => { println!("event: {:?}", event); }
+                Err(e) => { println!("watch error: {:?}", e); }
+            }
+        }
+    }
+}
+
+
+
+#[tokio::main]
+async fn main() { tokio::join!(sse_file_watch(), local_file_watch()); }
